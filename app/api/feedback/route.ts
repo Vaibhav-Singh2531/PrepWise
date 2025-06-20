54eaf8db24bf133f798c391a2825a746327067a8
Vaibhav-Singh2531 <vaibhavsingh2531@gmail.com>
2025-06-20 21:45:56 +0530

    Add retake interview

--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -25,21 +25,34 @@ export async function POST(req: Request) {
         structuredOutputs: false,
       }),
       schema: feedbackSchema,
-      prompt: `You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories...
-      
-      Transcript:
-      ${formattedTranscript}
+      prompt: `You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+        Transcript:
+        ${formattedTranscript}
 
-      Please score the candidate from 0 to 100 in the following areas:
-      - Communication Skills
-      - Technical Knowledge
-      - Problem-Solving
-      - Cultural & Role Fit
-      - Confidence & Clarity
-      `,
+        Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+        - **Communication Skills**: Clarity, articulation, structured responses.
+        - **Technical Knowledge**: Understanding of key concepts for the role.
+        - **Problem-Solving**: Ability to analyze problems and propose solutions.
+        - **Cultural & Role Fit**: Alignment with company values and job role.
+        - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+        `,
       system:
-        'You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories.',
+        'You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories',
     });
+
+
+    // Check for existing feedback with userId and interviewId, limit to 1 document
+    const querySnapshot = await db.collection('feedback')
+      .where('userId', '==', userId)
+      .where('interviewId', '==', interviewId)
+      .limit(1)
+      .get();
+
+    // Delete the existing feedback if found
+    if (!querySnapshot.empty) {
+      const existingDoc = querySnapshot.docs[0];
+      await existingDoc.ref.delete();
+    }
 
     const feedback = await db.collection('feedback').add({
       interviewId,
